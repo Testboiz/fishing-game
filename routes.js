4034aac674eb7b1c767ea7d0dd0f34775d80bf35
Testboiz <tvmini0202@gmail.com>
2024-07-09 16:07:38 +0700

    basic casting behavior completed

--- routes.js
+++ routes.js
@@ -7,7 +7,8 @@ db.pragma('journal_mode = WAL');
 function handleDBError(err, res){
     jsonOutput = {
         message : "the database blew up",
-        status : 500
+        status : 500,
+        debugMsg : err.message
     };
     res.status(jsonOutput["status"]).json(jsonOutput);
 }
@@ -20,6 +21,35 @@ function generateJSONSkeleton(objectOrMessage, httpCode){
     };
 }
 
+function formatMultilineStringToNormal(inputString) {
+    const escapedString = inputString.replace(/\n/g, '\\n');
+    return escapedString;
+}
+
+
+function ensureParametersOrValueNotNull(paramObject){
+    if (paramObject === null || paramObject === undefined){
+        throw new Error("The value is null or undefined");
+    }
+    for (let key in paramObject) {
+        if (paramObject[key] === null || paramObject[key] === undefined) {
+            throw new Error("One of the params are null or undefined");
+        }
+    }
+}
+
+function buoyLogin(buoy_id, rod_uuid){
+    try{
+        const query = "UPDATE rod_info SET buoy_fished = ? WHERE rod_uuid = ?";
+        ensureParametersOrValueNotNull(buoy_id);
+        const stmt = db.prepare(query);
+        stmt.run(buoy_id, rod_uuid); 
+    }
+    catch (err) {
+        throw err
+    }
+}
+
 router.get("/", function(_, res){
     res.json(generateJSONSkeleton("Server is up!", 200));
 });
@@ -30,8 +60,15 @@ router.get("/auth", function(req,res){
     const stmt = db.prepare(query);
 
     try{
+        const params = {
+            id : req.query.id,
+            username : req.query.username
+        };
+
+        ensureParametersOrValueNotNull(params);
+
         // parameters can be immmediately puy on the function, making the code cleaner
-        const result = stmt.get(req.query.id,req.query.username);
+        const result = stmt.get(params.id,params.username);
         var jsonOutput = {}
 
         if (result){
@@ -62,30 +99,18 @@ router.get("/fish", function(_, res){
     }
 });
 
-router.put("/buoy/connect", function(req,res){
-    try{
-        const query = "UPDATE rod_info SET selected_buoy = ?";
-        const buoy_id = req.query.buoyID;
-        const stmt = db.prepare(query);
-        stmt.run(buoy_id); 
-    }
-    catch (err){
-        handleDBError(err,res);
-    }
-});
-
-
-
-router.put("buoy/cast", function(req, res){
+router.put("/cast", function(req, res){
+    const TEMP_XP = 1;
+    const FISHPOT_RATE = 0.01;
     const sqlForFish = `
 WITH probability AS (
     SELECT ABS(RANDOM() / CAST(-9223372036854775808 AS REAL)) AS probability
 )
 SELECT f.fish_name, f.fish_value, c.casts FROM fish AS f, probability AS pr, fish_probability AS p, buoy_casts AS c
     JOIN fish_probability ON (f.fish_probability_class = p.probability_class)
-    JOIN probability ON pr.probability < p.probability_value OR f.fish_probability_class = "Common"
+    JOIN probability ON pr.probability < p.probability_value OR f.fish_probability_class = 'Common'
     JOIN buoy_casts ON c.buoy_uuid = ? AND c.player_username = ?
-    WHERE (pr.probability < p.probability_value OR f.fish_probability_class = "Common") 
+    WHERE (pr.probability < p.probability_value OR f.fish_probability_class = 'Common') 
     ORDER BY RANDOM()
     LIMIT 1       
 ;
@@ -93,26 +118,31 @@ SELECT f.fish_name, f.fish_value, c.casts FROM fish AS f, probability AS pr, fis
     const sqlForRank = `
 WITH ranked_fishers AS (
 SELECT
-    player_username,
-    player_display_name,
-    xp,
-    RANK() OVER (ORDER BY xp DESC) AS rank
+    rank_overall.player_username,
+    player.player_display_name,
+    player.linden_balance,
+    rank_overall.xp,
+    RANK() OVER (ORDER BY rank_overall.xp DESC) AS rank
 FROM
     rank_overall
+LEFT JOIN 
+    player ON rank_overall.player_username = player.player_username
 )
 SELECT
     ro1.player_username,
     ro1.player_display_name,
+    ro1.linden_balance,
     ro1.xp,
     ro1.rank,
     ro2.player_display_name AS above_display_name,
     ro2.xp - ro1.xp AS xp_difference,
     ro2.rank AS above_rank
 FROM
-    ranked_fishers ro1
-LEFT JOIN 
-    ranked_fishers ro2 ON ro1.rank = ro2.rank + 1
+    ranked_fishers AS ro1
+LEFT JOIN -- self join
+    ranked_fishers AS ro2 ON ro1.rank = ro2.rank + 1
 WHERE ro1.player_username = ?;
+
     `;
     const sqlForWorms = `
 UPDATE rod_info 
@@ -123,13 +153,17 @@ SET
     magic_worms = CASE WHEN selected_worm = 4 THEN magic_worms - 1 ELSE magic_worms END
 WHERE rod_uuid = ?;
     `;
+    const sqlWormInfo = `
+SELECT small_worms, tasty_worms, enchanted_worms, magic_worms FROM rod_info WHERE rod_uuid = ?;
+    `;
     const sqlForBuoys = `
 UPDATE buoy 
     SET 
     buoy_balance = buoy_balance - ?,
-    fishpot = fishpot +  ? * 0.01
+    fishpot = fishpot +  ?
 WHERE buoy_uuid = ?;
     `;
+    const sqlForUpdateRank = `UPDATE rank_overall  SET xp = xp + ? WHERE player_username = ?; `
     const sqlCastHandling = `
 INSERT INTO buoy_casts (buoy_uuid, player_username, casts) VALUES (?,?,0)
     ON CONFLICT (buoy_uuid, player_username) DO UPDATE SET casts = casts + 1;
@@ -138,36 +172,86 @@ INSERT INTO buoy_casts (buoy_uuid, player_username, casts) VALUES (?,?,0)
     const sqlUpdateAfterCast = `
 UPDATE player 
 SET 
-    xp = xp + ?,
     linden_balance = linden_balance + ?
     WHERE player_username = ?;
     `
     try{
-        const player_username = req.query.player_username;
-        const buoy_uuid = req.query.buoy_uuid;
-        const rod_uuid = req.query.rod_uuid;
+        const params = {
+            player_username : req.query.player_username,
+            buoy_uuid : req.query.buoy_uuid,
+            rod_uuid : req.query.rod_uuid
+        };
+
+        ensureParametersOrValueNotNull(params);
 
         const stmtFish = db.prepare(sqlForFish);
         const stmtRank = db.prepare(sqlForRank);
         const stmtWorm = db.prepare(sqlForWorms);
+        const stmtWormInfo = db.prepare(sqlWormInfo);
         const stmtBuoy = db.prepare(sqlForBuoys);
+        const stmtForUpdateRank = db.prepare(sqlForUpdateRank);
         const stmtCastHandling = db.prepare(sqlCastHandling);
         const stmtupdateAfterCast = db.prepare(sqlUpdateAfterCast);
 
-        stmtCastHandling.run(buoy_uuid,player_username);
-        stmtWorm.run(rod_uuid);
+        buoyLogin(params.buoy_uuid, params.rod_uuid);
+
+        stmtCastHandling.run(params.buoy_uuid,params.player_username);
+        stmtWorm.run(params.rod_uuid);
         
         // TODO handle spook
-        const fishCaught = stmtFish.get();
-        const rankInfo = stmtRank.get(player_username);
+        const fishCaught = stmtFish.get(params.buoy_uuid,params.player_username);
+        const wormInfo = stmtWormInfo.get(params.rod_uuid);
 
         // if error, buoy empty
-        stmtBuoy.run()
+        stmtBuoy.run(fishCaught.fish_value, fishCaught.fish_value * FISHPOT_RATE, params.buoy_uuid)
+        
+        stmtForUpdateRank.run(TEMP_XP, params.player_username);
+        stmtupdateAfterCast.run(fishCaught.fish_value,params.player_username)
 
-        stmtupdateAfterCast.run()
+        const rankInfo = stmtRank.get(params.player_username);
 
-        var message = "";
-        res.json(generateJSONSkeleton(message,200));
+        const debugObj = {
+            worms : {
+                small : wormInfo.small_worms,
+                tasty : wormInfo.tasty_worms,
+                enchanted : wormInfo.enchanted_worms,
+                magic : wormInfo.magic_worms
+            },
+            fish : fishCaught.fish_name,
+            xp : rankInfo.xp,
+            earnings : {
+                linden_balance : rankInfo.linden_balance,
+                fish_value : fishCaught.fish_value
+            },
+            rank_info : {
+                rank : rankInfo.rank,
+                xp_difference : rankInfo.xp_difference,
+                above_display_name : rankInfo.above_display_name,
+                above_rank : rankInfo.above_rank
+            }
+        };
+        var message = `
+Small Worms: ${wormInfo.small_worms}
+Tasty Worms: ${wormInfo.tasty_worms}
+Enchanted Worms: ${wormInfo.enchanted_worms}
+Magic Worms: ${wormInfo.magic_worms}
+Gold : Coming Soon!
+
+You caught ${fishCaught.fish_name}
+Fishing Exp: ${rankInfo.xp} (+1)
+Fish: Coming Soon!
+Your Earnings: ${rankInfo.linden_balance} L$ (+${fishCaught.fish_value}) 
+RANK Extras!: Coming Soon!
+Kingdoms Coming Soon! 
+
+RANK (overall):  ${rankInfo.rank}
+${rankInfo.xp_difference} XP to beat ${rankInfo.above_display_name}  ranked ${rankInfo.above_rank}.
+
+RANK (monthly):  Coming Soon!
+        `;
+        // res.json(generateJSONSkeleton(formatMultilineStringToNormal(message),200));
+        // for debug visualization
+        res.json(generateJSONSkeleton(debugObj,200));
     }
     catch(err){
         // TODO handle empty buoy here
